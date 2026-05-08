5ce8c09b671120f3da8859d85569585a23cd395a
kavizzb661-nick <kavishkab661@gmail.com>
2026-05-08 12:26:14 +0530

    Fixed

---
+++ Lab 3/Lab 1/Lab 1 Q4/Lab 1 Q4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_Q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Item price : ");//Get by user input one Item.
+            double numItem = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the Quantity you purchased : ");//Get by user input how many Items.
+            double quantity = double.Parse(Console.ReadLine());
+
+            double total = numItem * quantity;//Calculate the total Cost.
+
+            Console.WriteLine("Your Total cost is Rs." + total + ".00");//Display the Final result.        
+
+
+        }
+    }
+}
