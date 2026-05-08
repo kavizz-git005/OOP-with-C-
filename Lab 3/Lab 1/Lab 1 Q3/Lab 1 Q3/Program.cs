39f9b9eaf46f67545936fd17d8628429ac78c46c
kavizz-git005 <kavishkab661@gmail.com>
2026-05-08 16:37:18 +0530

    Work

---
+++ Lab 3/Lab 1/Lab 1 Q3/Lab 1 Q3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter NUmber 1 : ");//Get by user input Number 1.
+            int num1 = Int16.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Number 2 : ");//Get by user input Number 1.
+            int num2 = Int16.Parse(Console.ReadLine());
+
+            int calculate = num1 + num2;//Calculate.
+
+            Console.WriteLine("Final Answer: " + calculate);//Final result show.
+        }
+    }
+}
---
+++ Lab 3/Lab 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    internal class Program
+    {
+        static int Calfac(int num)
+        {
+            int factorial = 1;
+            for
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Positive Number");
+            int number = Int16.Parse(Console.ReadLine());
+            int result = Calfact(number);
+            Console.WriteLine("Factorial "+ )
+        }
+    }
+}
