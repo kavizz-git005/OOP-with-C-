7aa45dd59251b215267ad254a1e4ceff924d6ab1
kavizzb661-nick <kavishkab661@gmail.com>
2026-05-12 13:40:11 +0530

    Test

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab_3_Q2
+namespace Test
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Puka sududa ");
+            Console.WriteLine("Hello world");
 
             Console.ReadLine();
         }
