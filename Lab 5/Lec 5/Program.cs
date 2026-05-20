5b6cd4ce7949867bd7f8191f6e25f04ab2a46098
kavizzb661-nick <kavishkab661@gmail.com>
2026-05-20 09:44:50 +0530

    Free time

---
+++ Lab 5/Lec 5/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_5
+{  
+    class dog
+    {
+        string name;
+        public dog(string dogName)
+        {
+            name = dogName;
+            Console.WriteLine("dog name is: " + name);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dog d1 = new dog("babbi");
+            dog d2 = new dog("suuti");
+            Console.ReadLine();
+        }
+    }
+}
